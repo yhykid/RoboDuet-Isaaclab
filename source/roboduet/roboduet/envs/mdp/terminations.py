ab293ee5b4b56561dca54b0d8d3af4ebbeaae5e1
young <yhykid@mail.ustc.edu.cn>
2025-12-05 11:00:44 +0800

    terminations

--- source/roboduet/roboduet/envs/mdp/terminations.py
+++ source/roboduet/roboduet/envs/mdp/terminations.py
@@ -18,7 +18,8 @@ from isaaclab.assets import Articulation
 from isaaclab.managers import SceneEntityCfg
 from isaaclab.utils.math  import euler_xyz_from_quat, wrap_to_pi
 from roboduet.envs.mdp import DuetEvent
- 
+from roboduet.utils.switch import global_switch
+
 if TYPE_CHECKING:
     from roboduet.envs import DuetManagerBasedRLEnv
 
@@ -29,15 +30,23 @@ def terminate_episode(
     reset_buf = torch.zeros((env.num_envs, ), dtype=torch.bool, device=env.device)
     asset: Articulation = env.scene[asset_cfg.name]
     roll, pitch, _ = euler_xyz_from_quat(asset.data.root_state_w[:,3:7])
-    roll_cutoff = torch.abs(wrap_to_pi(roll)) > 1.5
-    pitch_cutoff = torch.abs(wrap_to_pi(pitch)) > 1.5
+
     time_out_buf = env.episode_length_buf >= env.max_episode_length
-    duet_event: DuetEvent =  env.duet_manager.get_term('base_duet')    
-    reach_goal_cutoff = duet_event.cur_goal_idx >= env.scene.terrain.cfg.terrain_generator.num_goals
-    height_cutoff = asset.data.root_state_w[:, 2] < -0.25
-    time_out_buf |= reach_goal_cutoff
+    height_cutoff = asset.data.root_state_w[:, 2] < 0.28
     reset_buf |= time_out_buf
-    reset_buf |= roll_cutoff
-    reset_buf |= pitch_cutoff
     reset_buf |= height_cutoff
+    
+    reverse_buf = torch.zeros(env.num_envs, dtype=torch.bool, device=env.device, requires_grad=False)
+    commands_dog,commands_arm = env.command_manager.get_command('base_velocity')
+    p_align = commands_arm[:, 1]
+    l_align = commands_arm[:, 0]
+    delta_z = l_align*torch.sin(p_align) + 0.38 - asset.data.root_state_w[:, 2]
+    if global_switch.switch_open :
+        reverse_buf1 = torch.logical_and(pitch < -0.2, delta_z < -0.1) # lpy
+        reverse_buf2 = torch.logical_and(pitch > 0.2, delta_z > 0.1) # lpy
+        reverse_buf |= reverse_buf1 | reverse_buf2
+        time_exceed_half = (env.arm_time_buf / (env.T_trajs / env.step_dt)) > 0.6
+        reverse_buf = reverse_buf & time_exceed_half
+        reset_buf |= reverse_buf
+    
     return reset_buf
